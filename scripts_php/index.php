8051b14e9a63a757edb916997d47df4d89a7e610
Carlos Zaragoza Beato <carloszaragozabeato15@gmail.com>
2023-05-02 11:53:46 +0200

    02-05_comienzo

---
+++ scripts_php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Python Scripts</title>
+</head>
+<body>
+
+    <header> 
+        <h1>Python Scripts</h1>
+    </header>
+
+    <form method="GET" action="index.php">
+            <input type="checkbox" value="Google" name="google"/>
+    </form>
+
+
+
+
+    
+</body>
+</html>
